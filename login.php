e96816fa832d135a64b92a69fcf1eebafd974c6b
Silvalore <160123568+Silvalore@users.noreply.github.com>
2026-05-05 11:25:04 +0200

    Add login functionality to login.php
    Implement basic login functionality with session management.

--- login.php
+++ login.php
@@ -1,1 +1,43 @@
+<?php
+session_start();
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user = $_POST['username'];
+    $password = $_POST['password'];
+    
+    
+    if ($user === 'lorenzo' && $password === 'verifica') {
+        $_SESSION['username'] = $user;
+        $_SESSION['logged_in'] = true;
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = "Username o password errati!";
+    }
+}
+
+
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+    header("Location: index.php");
+    exit();
+}
+?>
+
+?>
+
+<!DOCTYPE html>
+<html lang ="en">
+
+    <head>
+
+        <title>Palestra Silva</title>
+    
+    </head>
+
+    <body>
+
+    </body>
+
+    </html>
+
+   
